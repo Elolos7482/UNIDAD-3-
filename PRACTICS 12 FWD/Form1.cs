8d53dab1353537bb998b00f621d07e6f2e0006f2
Elolos7482 <cadedward08@gmail.com>
2025-04-04 23:42:50 -0500

    Add files via upload

---
+++ PRACTICS 12 FWD/Form1.cs
@@ -0,0 +1,21 @@
+namespace PRACTICS_12_FWD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double cal;
+            cal = double.Parse(textBox1.Text);
+            if (cal < 3.0) MessageBox.Show(" REPROBADO ");
+        }
+    }
+}
