f1b9b3ffe7f132de20417808cc1a1bd58877a496
Elolos7482 <cadedward08@gmail.com>
2025-04-04 23:44:15 -0500

    Add files via upload

---
+++ PACTRICA 15 FWD/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace PACTRICA_15_FWD
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(76, 51);
+            label1.Name = "label1";
+            label1.Size = new Size(382, 75);
+            label1.TabIndex = 0;
+            label1.Text = "Dado coomo dato el sueldo de un trabajador, \r\nel programa aplica un aumento del 15% \r\nsi el sueldo es inferior a 400000";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(76, 196);
+            label2.Name = "label2";
+            label2.Size = new Size(136, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Ingresar Sueldo";
+            label2.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(293, 194);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(134, 31);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(76, 269);
+            button1.Name = "button1";
+            button1.Size = new Size(166, 44);
+            button1.TabIndex = 3;
+            button1.Text = "Calcular ";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(307, 269);
+            button2.Name = "button2";
+            button2.Size = new Size(120, 44);
+            button2.TabIndex = 4;
+            button2.Text = "Borrar ";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(298, 357);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(132, 31);
+            textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(698, 450);
+            Controls.Add(textBox2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox2;
+    }
+}
---
+++ PRACTICAS 16 FWD/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace PRACTICAS_16_FWD
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(202, 38);
+            label1.Name = "label1";
+            label1.Size = new Size(266, 75);
+            label1.TabIndex = 0;
+            label1.Text = "Dado como un dato un numero\r\n determinar si es par o impar y \r\ndivisible por 3";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(34, 201);
+            label2.Name = "label2";
+            label2.Size = new Size(228, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Ingresar numero cualquiera";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(353, 203);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(167, 31);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(151, 301);
+            button1.Name = "button1";
+            button1.Size = new Size(126, 31);
+            button1.TabIndex = 3;
+            button1.Text = "Determinar ";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(376, 300);
+            button2.Name = "button2";
+            button2.Size = new Size(105, 32);
+            button2.TabIndex = 4;
+            button2.Text = "Borrar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(688, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
